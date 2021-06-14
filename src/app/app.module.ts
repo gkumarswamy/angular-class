ccf5d9a48ab9c8152dfde72d5dee69347ca8dc87
yharithareddy <harithareddy920@gmail.com>
2021-06-14 17:56:09 +0530

    site moduloe done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SiteModule } from './site/site.module'
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SiteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/site/site.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'', component:HomeComponent},
+];
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    RouterModule
+  ]
+})
+export class SiteModule { }
